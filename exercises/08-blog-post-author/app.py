f292b0e31a1f0eb1be1e062797944d59a5893d0c
José Ignacio Ibarra Lemp <jiibarra@uc.cl>
2025-02-24 17:11:16 +0000

    Exercises 1 to 8

--- exercises/08-blog-post-author/app.py
+++ exercises/08-blog-post-author/app.py
@@ -1,3 +1,6 @@
 import requests
 
 # Your code here
+response = requests.get('https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php')
+response_dict = response.json()
+print(response_dict['posts'][0]['author']['name'])
